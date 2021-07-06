78f829906242ee40bef579816f33c8a65a9ac873
Byurhan Beyzat <imbyurhan@gmail.com>
2021-07-06 11:32:57 +0300

    Change chain to ae from eth

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let debug = false;
 
 let baseUrl = 'http://localhost:4000';
 let token = null;
-let network = "eth"; //ae,eth
+let network = "ae"; //ae,eth
 
 let defaultRequestId = 'ReCheck';
 const pollingTime = 90;
